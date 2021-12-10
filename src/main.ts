a20b5723764dc8af738aec7585a2ce6d944d684b
ayurash-77 <ayurash@me.com>
2021-12-10 22:32:55 +0300

    TAGS: init

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import * as colors from 'colors';
 
 const PORT = process.env.PORT || 3000;
-console.log(`➜ Server starting at port: ${PORT}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder() //
+    .setTitle('PP18 PManager server')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(PORT);
 }
 bootstrap().then(() => {
-  console.log(`➜ Server ready at port: ${PORT}`);
+  console.log(colors.blue(`Server ready on port: ${PORT}`));
 });
