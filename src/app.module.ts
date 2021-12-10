b0c40bae392daab1a933a367388e25cc8951dbf5
ayurash-77 <ayurash@me.com>
2021-12-10 21:54:03 +0300

    TAGS: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
